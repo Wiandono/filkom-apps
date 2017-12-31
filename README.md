5d54ac1ecd383c254a41f38620255799de41cd6d
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-12-31 17:57:56 +0700

    Initial Release

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-filkom-apps
+![Logo](/logo.png)
+
+# Filkom Apps
+> Untuk sementara hanya akan mendukung pengecekan jadwal kuliah
+
+### Expected features (?)
+- Changing between light and dark theme
+- Include all other Filkom Apps API (Kalo gabut)
---
+++ www/js/main.js
@@ -0,0 +1,43 @@
+var header = "<li class='ui-screen-hidden'><a>"; 
+var footer = "</a></li>"; 
+var result = ''; 
+var hari = ''; 
+var praktikum = ''; 
+ 
+$.ajax({ 
+    url: 'http://filkom.ub.ac.id/module/api/conf/get_jadwal/in', 
+    type: 'get', 
+    success: function (data) { 
+        objP = JSON.parse(atob(data)); 
+ 
+        $.each(objP, function (index, value) { 
+            if (index == "senin") { 
+                hari = "Senin"; 
+            } else if (index == "selasa") { 
+                hari = "Selasa"; 
+            } else if (index == "rabu") { 
+                hari = "Rabu"; 
+            } else if (index == "kamis") { 
+                hari = "Kamis"; 
+            } else { 
+                hari = "Jumat"; 
+            } 
+            $.each(value, function (index2, value2) { 
+                $.each(value2, function (index3, value3) { 
+                    if (value3.praktikum == "1") { 
+                        praktikum = " | Praktikum" 
+                    } else { 
+                        praktikum = ""; 
+                    } 
+                    result += header; 
+                    result += "<h2>" + value3.matakuliah + " - " + value3.kelas + "</h2>"; 
+                    result += "<p><strong>" + hari + " | " + value3.jam_mulai + " - " + value3.jam_selesai + " | " + index2 + praktikum + "</strong></p>"; 
+                    result += "<p>" + value3.dosen + "</p>"; 
+                    result += footer; 
+                }) 
+            }); 
+        }); 
+ 
+        document.getElementById('listview').innerHTML = result; 
+    } 
+});
